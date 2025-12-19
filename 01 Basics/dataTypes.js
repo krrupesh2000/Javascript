f2d248585efd831c473198bdfbe26b3448934de2
krrupesh2000 <111333691+krrupesh2000@users.noreply.github.com>
2025-12-19 03:25:46 +0000

    datatypes

---
+++ 01 Basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+
+let name = "Rupesh"
+let age = 22
+let isLoggedIn = true
+
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned
+symbol => unique
+
+
+object
+*/ 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
