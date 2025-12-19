1d87b66840cfa18c929aa16946468b2b181190dd
krrupesh2000 <111333691+krrupesh2000@users.noreply.github.com>
2025-12-19 03:49:51 +0000

    conversion

---
+++ 01 Basics/conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 55
+let runs = "120"
+let wicket = "5w"
+let over = null
+
+console.log(typeof score);
+console.log(typeof(score));
+console.log(typeof runs);
+
+let valueInNumber = Number (runs)
+console.log(typeof valueInNumber);
+
+let value_in_number = Number (wicket)
+console.log(typeof value_in_number);
+console.log(value_in_number);
+
+let value_In_Number = Number (over)
+console.log(typeof value_In_Number);
+console.log(value_In_Number);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean( isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//"33" => 33
+//"5w" => NaN
+// true => 1, false => 0
+// "" => false
+//"Rupesh" => true
